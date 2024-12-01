9c7652dc7789087207fa4ad1eb7f858617cefce3
shailja-07 <2004shailja@gmail.com>
2024-12-01 13:52:34 +0530

    file added

---
+++ app.py
@@ -0,0 +1,43 @@
+from langchain.chains.llm import LLMChain
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.llms import HuggingFaceHub
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+
+
+model_name = "Qwen/Qwen2.5-Coder-32B-Instruct"
+llm = HuggingFaceHub(repo_id=model_name, huggingfacehub_api_token="api_key")
+
+prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "Generate information in one complete bullet point for the following topic:\n\n{context}")
+        ]
+    )
+    
+    
+llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+
+app = FastAPI()
+
+
+class Data(BaseModel):
+    text: str
+
+@app.get("/")
+@app.head("/")
+def root():
+    return {"message": "Welcome!"}
+
+@app.post("/predict")
+def predict(data: Data):
+    input_data = data.text 
+    
+    if input_data:
+        info_text=llm_chain.run(input_data)
+    else:
+        print("enter topic")
+    return {"information": info_text}  
+
+
